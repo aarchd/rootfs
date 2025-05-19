f7c014b435a1ab10c55c0f4a948efbe41598e6a6
Deepak Meena <notwho53@gmail.com>
2025-05-19 20:44:00 +0530

    setup-aarchd-repo: give aarchd repo prio over others
    Signed-off-by: Deepak Meena <notwho53@gmail.com>

--- setup-aarchd-repo.sh
+++ setup-aarchd-repo.sh
@@ -10,8 +10,19 @@ pacman-key --lsign-key 6290731A5B773E16
 pacman -U 'https://aarchd.who53.me/repo/aarchd-keyring-1.0-1-any.pkg.tar.zst' --noconfirm
 pacman -U 'https://aarchd.who53.me/repo/aarchd-mirrorlist-1.0-1-any.pkg.tar.zst' --noconfirm
 
-echo "[aarchd]" >> /etc/pacman.conf
-echo "Include = /etc/pacman.d/aarchd-mirrorlist" >> /etc/pacman.conf
+if ! grep -q '^\[aarchd\]' /etc/pacman.conf; then
+    if core_line=$(grep -n '^\[core\]' /etc/pacman.conf | cut -d: -f1); then
+        insert_line=$((core_line - 1))
+        sed -i "${insert_line}i\\
+\\
+[aarchd]\\
+Include = /etc/pacman.d/aarchd-mirrorlist
+" /etc/pacman.conf
+    else
+        echo "Error: Could not find [core] repo in /etc/pacman.conf" >&2
+        exit 1
+    fi
+fi
 
 # set SigLevel to default
 sed -i "s/^[[:space:]]*SigLevel[[:space:]]*=.*$/SigLevel = Required DatabaseOptional/" "/etc/pacman.conf"
